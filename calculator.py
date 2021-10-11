c0303b5a4d0fc3d1fed033a15d61ca49cb5ddf13
speakitk <92300012+speakitk@users.noreply.github.com>
2021-10-11 19:38:46 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,3 @@
+height = float(input("Input your height in Feet: "))
+weight = float(input("Input your weight in Kilogram: "))
+print("Your body mass index is: ", round(weight / (height * height), 2))
